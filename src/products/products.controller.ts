497c019f45957520c631bf8b13030a8e9f41c618
Michael Guay <michaelguay@Michaels-Mac-Studio.local>
2024-05-02 21:04:58 -0400

    Single product

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -4,6 +4,7 @@ import {
   FileTypeValidator,
   Get,
   MaxFileSizeValidator,
+  Param,
   ParseFilePipe,
   Post,
   UploadedFile,
@@ -18,6 +19,7 @@ import { ProductsService } from './products.service';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { diskStorage } from 'multer';
 import { extname } from 'path';
+import { PRODUCT_IMAGES } from './product-images';
 
 @Controller('products')
 export class ProductsController {
@@ -37,7 +39,7 @@ export class ProductsController {
   @UseInterceptors(
     FileInterceptor('image', {
       storage: diskStorage({
-        destination: 'public/products',
+        destination: PRODUCT_IMAGES,
         filename: (req, file, callback) => {
           callback(
             null,
@@ -63,5 +65,11 @@ export class ProductsController {
   @UseGuards(JwtAuthGuard)
   async getProducts() {
     return this.productsService.getProducts();
+  }
+
+  @Get(':productId')
+  @UseGuards(JwtAuthGuard)
+  async getProduct(@Param('productId') productId: string) {
+    return this.productsService.getProduct(+productId);
   }
 }
--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,8 +1,9 @@
 import { promises as fs } from 'fs';
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateProductRequest } from './dto/create-product.request';
 import { PrismaService } from '../prisma/prisma.service';
 import { join } from 'path';
+import { PRODUCT_IMAGES } from './product-images';
 
 @Injectable()
 export class ProductsService {
@@ -27,10 +28,23 @@ export class ProductsService {
     );
   }
 
+  async getProduct(productId: number) {
+    try {
+      return {
+        ...(await this.prismaService.product.findUniqueOrThrow({
+          where: { id: productId },
+        })),
+        imageExists: await this.imageExists(productId),
+      };
+    } catch (err) {
+      throw new NotFoundException(`Product not found with ID ${productId}`);
+    }
+  }
+
   private async imageExists(productId: number) {
     try {
       await fs.access(
-        join(__dirname, '../../', `public/products/${productId}.jpg`),
+        join(`${PRODUCT_IMAGES}/${productId}.jpg`),
         fs.constants.F_OK,
       );
       return true;
