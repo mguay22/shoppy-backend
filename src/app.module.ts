9a09d82529929a931a9ed496098c16aea06b95e8
Michael <guay.me@gmail.com>
2024-06-01 10:11:37 -0400

    Add health controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,6 +7,7 @@ import { ProductsModule } from './products/products.module';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
 import { CheckoutModule } from './checkout/checkout.module';
+import { HealthController } from './health.controller';
 
 @Module({
   imports: [
@@ -40,7 +41,7 @@ import { CheckoutModule } from './checkout/checkout.module';
     ProductsModule,
     CheckoutModule,
   ],
-  controllers: [],
+  controllers: [HealthController],
   providers: [],
 })
 export class AppModule {}
